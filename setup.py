1479dc478dea033e294d50ce572b8764778adb61
Alex Saltzman <a.saltzman920@gmail.com>
2018-04-13 12:13:33 -0500

    add geneignore txt file to data

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # correlationplot=main:main
     """,
 
-    package_data={'': ['R/*.r', 'R/*.R', 'GSEA/*.jar', 'GSEA/*.gmt', 'data/*.data', 'data/*txt]},
+    package_data={'': ['R/*.r', 'R/*.R', 'GSEA/*.jar', 'GSEA/*.gmt', 'data/*.data', 'data/*txt']},
     include_package_data=True
 
 )
