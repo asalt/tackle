0068cc9c52b24a994b04de8f220e8d621e0d5439
asalt <a.saltzman920@gmail.com>
2019-08-13 11:44:01 -0500

    improve tackle metrics plot

--- tackle/R/metrics.R
+++ tackle/R/metrics.R
@@ -13,7 +13,9 @@ metrics <- function(df, savename=NULL, exts=NULL, ...){
 
   df_sra<- df[,c('Sample', 'S', 'R', 'A')] %>%
     gather(S, R, A, key='SRA_metric', value='SRA')
-  df_sra$SRA_metric <- factor(df_sra$SRA_metric, levels=c('A', 'R', 'S'))
+  df_sra$SRA_metric <- factor(df_sra$SRA_metric, levels=c('S', 'R', 'A'),
+                              ordered=TRUE)
+  df_sra <- df_sra %>% group_by(Sample) %>% mutate(  cumsum = cumsum(SRA) )
 
   df_psms<- df[, c('Sample', 'Total_psms', 'u2g_psms')] %>%
     gather(Total_psms, u2g_psms, key='PSM_metric', value='PSMs')
@@ -26,23 +28,35 @@ metrics <- function(df, savename=NULL, exts=NULL, ...){
                                     levels=c('Total_peptides', 'u2g_peptides',
                                              'Strict', 'Strict_u2g'))
 
+  thewidth <- dim(df)[1] %/% 2 %>% max(9) %>% min(21)
+  annot_scale = (thewidth/9)
 
   ## green = 'darkgreen'; yellow = 'gold'; red ='firebrick'
 
-  p0 <- ggplot(data=df_sra, aes(x=factor(Sample, levels=df$Sample), y=SRA, fill=SRA_metric)) +
+  p0 <- ggplot(data=df_sra, aes(x=factor(Sample, levels=df$Sample),
+                                y=SRA,
+                                fill=factor(SRA_metric, levels=c('A', 'R', 'S')))) +
     geom_bar(stat="identity") +
     scale_fill_manual(values=c('firebrick', 'gold', 'darkgreen')) +
+    geom_text(aes(y=cumsum, label=SRA, color=SRA_metric), size=1.44*annot_scale,
+              vjust=1.6, show.legend=FALSE) +
+    scale_colour_manual(values=c("white", "black", 'white')) +
+    theme_classic() +
     theme(text = element_text(size=14),
           legend.position = 'top',
           axis.text.x=element_blank(),
           legend.title=element_text(size=8),
           legend.text = element_text(size=8),
           ) +
+    labs(fill='SRA\nMetric') +
   xlab(NULL)
 
 
   p1 <- ggplot(data=df, aes(x=factor(Sample, levels=df$Sample), y=GPGroups)) +
     geom_bar(stat="identity") +
+    geom_text(aes(label=GPGroups), vjust=1.6, color="white", size=1.24*annot_scale,
+              show.legend=FALSE) +
+    theme_classic() +
     theme(text = element_text(size=12),
           axis.text.x=element_blank(),
           ) +
@@ -51,23 +65,29 @@ metrics <- function(df, savename=NULL, exts=NULL, ...){
 
   p2 <- ggplot(data=df_psms, aes(x=factor(Sample, levels=df$Sample), y=PSMs, fill=PSM_metric)) +
     geom_bar(stat="identity", position=position_dodge()) +
+    theme_classic() +
     theme(text = element_text(size=14),
           legend.position = 'top',
           legend.title=element_text(size=8),
           legend.text = element_text(size=8),
           axis.text.x = element_text(angle=90, hjust=1, vjust=.4)
           ) +
+    guides(fill=guide_legend(nrow=1, byrow=TRUE)) +
+    labs(fill='PSM\nMetric') +
     xlab(NULL)
 
 
   p3 <- ggplot(data=df_pepts, aes(x=factor(Sample, levels=df$Sample), y=Peptides, fill=Peptide_metric)) +
     geom_bar(stat="identity", position=position_dodge()) +
+    theme_classic() +
     theme(text = element_text(size=14),
           legend.position = 'top',
           legend.title=element_text(size=8),
           legend.text = element_text(size=8),
           axis.text.x = element_text(angle=90, hjust=1, vjust=.4)
           ) +
+    guides(fill=guide_legend(nrow=1, byrow=TRUE)) +
+    labs(fill='Peptide\nMetric') +
     xlab(NULL)
 
 
@@ -83,8 +103,6 @@ metrics <- function(df, savename=NULL, exts=NULL, ...){
 
       outname <- paste(savename, ext, sep='.')
 
-      thewidth <- dim(df)[1] %/% 3 %>% max(5) %>% min(16)
-      print(thewidth)
       ggsave(outname, height=9, width=thewidth, units='in')
 
     }
