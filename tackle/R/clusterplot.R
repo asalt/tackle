70bccdc7ec7612e6d29dfd941fa2cc6108c351f4
Alex Saltzman <a.saltzman920@gmail.com>
2021-04-01 11:56:44 -0500

    edge case for all na

--- tackle/R/clusterplot.R
+++ tackle/R/clusterplot.R
@@ -8,12 +8,15 @@ myzscore <- function(value, minval = NA, remask = TRUE) {
   mask <- is.na(value)
   if (is.na(minval)) minval <- min(value, na.rm = TRUE)
 
+  if (minval == Inf) {
+    minval < 0
+  }
 
   value[is.na(value)] <- minval
   out <- scale(value)
 
   # if all NA:
-  if (sum(!is.finite(out)) == length(out)){
+  if (sum(!is.finite(out)) == length(out)) {
     out[,1] <- 0
   }
 
@@ -26,6 +29,8 @@ myzscore <- function(value, minval = NA, remask = TRUE) {
       }
     return(out)
 }
+
+
 
 dist_no_na <- function(mat) {
     mat[is.na(mat)] <- min(mat, na.rm = TRUE)*.9
