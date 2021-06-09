bb1e88505331ef57fb03bcd8b6f5595113b27b7c
Alex Saltzman <a.saltzman920@gmail.com>
2021-06-09 00:01:14 -0500

    require first column to be unique

--- tackle/Ini2Csv/main.py
+++ tackle/Ini2Csv/main.py
@@ -86,6 +86,8 @@ class Csv2Conf(object):
         self._rows = self._rows[1:]
 
     def _transform(self):
+        if len(row[0] for row in self._rows) != len(set(row[0] for row in self._rows) ):
+            raise ValueError('first column must be unique')
         for row in self._rows:
             obj = {}
             i = 1
