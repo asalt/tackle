4d70866c41661e6c8a95fa20e2bc3acb1e6a7918
Alex Saltzman <saltzman@bcm.edu>
2024-11-26 19:54:17 -0600

    slightly cleaner logic

--- tackle/R/barplot.R
+++ tackle/R/barplot.R
@@ -1,72 +1,101 @@
 ## library(plyr)
-library(dplyr)
-library(forcats)
-library(data.table)
-library(moments)
-library(ggplot2)
-library(ggpubr)
-library(paletteer)
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(forcats))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(moments))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(ggpubr))
+suppressPackageStartupMessages(library(colorspace))
+# suppressPackageStartupMessages(library(paletteer))
 
-barplot <- function(df, average = NULL, group = NULL, group_order = NULL, title = "", ylab = "log10 Expression") {
+barplot <- function(df, average = NULL, group = NULL, group_order = NULL, title = "", ylab = "log10 Expression", cmap=NULL) {
+  # Check if 'Expression' column exists
+  if(!"Expression" %in% colnames(df)) {
+    stop("The dataframe must contain an 'Expression' column.")
+  }
 
+  # Replace NA in Expression with 0
+  df <- df %>%
+    mutate(Expression = ifelse(is.na(Expression), 0, Expression))
 
-  ## dfl <- df %>% filter(GeneSymbol == gene) %>%
-  ##   select(-GeneID, -GeneSymbol) %>%
-  ##   t
-  ## colnames(dfl) <- 'Expression'
-  ## dfl <- dfl %>% merge(select(meta, PDX, E2.dependence, Tumor, treat), by=0)
+# Handle grouping and averaging
+  if (!is.null(group)) {
+    
+    # Check if the grouping column exists
+    if(!group %in% colnames(df)) {
+      stop(paste("The grouping column", group, "does not exist in the dataframe."))
+    }
+    
+    # If averaging is requested
+    if (!is.null(average)) {
+      df <- df %>%
+        group_by({{ group }}) %>%
+        summarize(
+          Expression = mean(Expression, na.rm = TRUE),
+          sd = sd(Expression, na.rm = TRUE),
+          .groups = 'drop'
+        ) %>%
+        rename(index = {{ group }})
+      group <- "index"  # Update group to the new summarized column
+      
+    } else {
+      # If group_order is provided, set factor levels accordingly
+      if (!is.null(group_order)) {
+        df <- df %>%
+          mutate({{ group }} := factor(!!sym(group), levels = group_order , ordered = TRUE))
+      } else {
+        # If no group_order, set the group as an ordered factor based on appearance
+        df <- df %>%
+          mutate({{ group }} := as.factor(!!sym(group)))
+      }
+      
+      # Optional: Create an 'index' for plotting if needed
+      # df <- df %>%
+      #   arrange({{ group }}) %>%
+      #   mutate(index = factor({{ group }}, levels = levels({{ group }}), ordered = TRUE))
+    }
+  } else {
+    # If no grouping is specified, ensure 'index' exists for plotting
+    # if(!"index" %in% colnames(df)) {
+    #   df <- df %>%
+    #     mutate(index = factor(1:n()))
+    # }
+  }
 
-  ## browser()
-
-  df[is.na(df$Expression), "Expression"] <- 0
 
   ## geom_dotplot(binaxis='y', stackdir='center',  dotsize=.5, position=position_dodge(0.8), alpha=.5) +
 
-  ## if (!is.na(group)) df[[group]] <- make.names(df[[group]]) %>% gsub('\\.', '', . )
-  ## if (!is.na(group_order)) group_order <- make.names(group_order) %>% gsub("\\.", "", .)
-  # browser()
-
   ## TODO fix when group is null
   ## I'm bad at reordering things
-  if (!is.null(group) && !is.null(average)) {
-    # the order is important here, sd before mean, because we're renaming Expression
-    df <- df %>%
-      group_by(get(group)) %>%
-      summarize(sd = sd(Expression), Expression = mean(Expression))
-    colnames(df)[1] <- "index"
-    group <- "index" # we rename it here since that's now our averaged column
-  }
-  else if (!is.null(group) && !is.null(group_order) && is.null(average)) {
-    df[[group]] <- factor(df[[group]], levels = group_order, ordered = TRUE)
-    ## df <- df %>% mutate(index=fct_reorder(index, as.numeric(get(group))))
-    df <- arrange(df, get(group))
-    df$index <- factor(df$index, levels = df$index, ordered = TRUE)
-  }
-   else if (!is.null(group_order) && !is.null(average)) {
-    df[[group]] <- factor(df[[group]], levels = group_order, ordered = TRUE)
-    df <- arrange(df, as.numeric(df$index))
-  } else if (!is.null(group) && is.null(group_order) && is.null(average)) {
-    # automatic ordering
-    df[[group]] <- factor(df[[group]], ordered = TRUE)
-    df <- df %>% arrange(!!!group)
-    ## TODO: fix this
-    ## df[['index']] <- factor(df[['index']], ordered = TRUE)
-    ## df <- arrange(df, as.numeric(df$index))
-    ## ??
-    df <- df %>% mutate(index = factor(index, ordered = TRUE))
-  }
+  # if (!is.null(group) && !is.null(average)) {
+  #   # the order is important here, sd before mean, because we're renaming Expression
+  #   df <- df %>%
+  #     group_by(get(group)) %>%
+  #     summarize(sd = sd(Expression), Expression = mean(Expression))
+  #   colnames(df)[1] <- "index"
+  #   group <- "index" # we rename it here since that's now our averaged column
+  # }
+  # else if (!is.null(group) && !is.null(group_order) && is.null(average)) {
+  #   df[[group]] <- factor(df[[group]], levels = group_order, ordered = TRUE)
+  #   ## df <- df %>% mutate(index=fct_reorder(index, as.numeric(get(group))))
+  #   df <- arrange(df, get(group))
+  #   df$index <- factor(df$index, levels = df$index, ordered = TRUE)
+  # }
+  #  else if (!is.null(group_order) && !is.null(average)) {
+  #   df[[group]] <- factor(df[[group]], levels = group_order, ordered = TRUE)
+  #   df <- arrange(df, as.numeric(df$index))
+  # } else if (!is.null(group) && is.null(group_order) && is.null(average)) {
+  #   # automatic ordering
+  #   df[[group]] <- factor(df[[group]], ordered = TRUE)
+  #   df <- df %>% arrange(!!!group)
+  #   ## TODO: fix this
+  #   ## df[['index']] <- factor(df[['index']], ordered = TRUE)
+  #   ## df <- arrange(df, as.numeric(df$index))
+  #   ## ??
+  #   df <- df %>% mutate(index = factor(index, ordered = TRUE))
+  # }
 
-
-  ## p <- ggplot(data = df, aes_string(x=factor(group), y="Expression", fill=group)) +
-  ## p <- ggplot(data = df, aes_string(x=fct_reorder('index', as.numeric(group_order)), y="Expression", fill=group)) +
-  ## p <- ggplot(data = df, aes_string(x='index', y="Expression", fill=group)) +
-
-  ## guides(fill=guide_legend(title=''), ) +
-
-
-  ## p <- ggplot(data = df, aes(x=factor(index, level=df$index), y=Expression, fill=get(group))) +
-  ## p <- ggplot(data = df, aes(x=index, y=Expression, fill=get(group))) +
-
+    # scale_fill_paletteer_d("ggthemes::calc") +
   ## scale_fill_manual(values = c("#999999", "#E69F00", "#56B4E9")) +
 
   p <- ggplot(data = df, aes_string(x = "index", y = "Expression", fill = group)) +
@@ -77,8 +106,8 @@ barplot <- function(df, average = NULL, group = NULL, group_order = NULL, title 
     guides(fill = guide_legend(title = "")) +
     labs(title = title, reverse = TRUE) +
     ylab(ylab) +
-    scale_fill_paletteer_d("ggthemes::calc") +
     theme_light() +
+    colorspace::scale_fill_discrete_qualitative(palette = "Dynamic") +
     theme(
       text = element_text(size = 16),
       legend.position = "bottom",
